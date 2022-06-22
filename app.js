15c9abe452c26bc9e1b97ae07b4622aaa6443edf
ChaabaneAnas <anas.chaabane98@gmail.com>
2022-06-22 14:20:54 +0100

    fix-linters

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-let humb = document.querySelector(".humb");
-let navMenu = document.querySelector(".nav");
-let close = document.querySelector(".closebtn");
-let menuElement = document.querySelector('.navL')
+const humb = document.querySelector('.humb');
+const navMenu = document.querySelector('.nav');
+const closeBtn = document.querySelector('.closebtn');
+const menuElement = document.querySelector('.navL');
 
-humb.addEventListener('click',function(e){
-navMenu.classList.add('active')
-})
+function active(){
+  navMenu.classList.add('active');
+}
+function uActive(){
+  navMenu.classList.remove('active');
+}
 
-close.addEventListener('click', function(e){
-    navMenu.classList.remove('active')
-})
+humb.addEventListener('click', active);
 
-menuElement.addEventListener('click', function(e){
-    navMenu.classList.remove('active')
-})
+closeBtn.addEventListener('click', uActive);
+   
+menuElement.addEventListener('click', uActive);
