48854de87fc11720a9a981601435279a613905f7
ChaabaneAnas <anas.chaabane98@gmail.com>
2022-06-23 18:05:27 +0100

    aid

--- app.js
+++ app.js
@@ -13,3 +13,19 @@ function uActive() {
 humb.addEventListener('click', active);
 closeBtn.addEventListener('click', uActive);
 menuElement.addEventListener('click', uActive);
+
+
+//popup-menu secion
+const popup = {
+  name: 'Keeping track of hundreds of components';
+  Desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea';
+  tech: ['Ruby on rails','css','JavScript']
+  mImage: 'src="images/SnapshootPortfolio.png"'
+}
+
+const popup = document.createElement('div');
+popup.classList.add('container');
+popup.
+
+
+
