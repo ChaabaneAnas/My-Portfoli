75e0e7e24656446834acd5fbad9b5333b0786dc9
ChaabaneAnas <anas.chaabane98@gmail.com>
2022-06-24 01:08:03 +0100

    first commit

--- app.js
+++ app.js
@@ -13,3 +13,31 @@ function uActive() {
 humb.addEventListener('click', active);
 closeBtn.addEventListener('click', uActive);
 menuElement.addEventListener('click', uActive);
+
+const data = {
+  Image: 'src="images/SnapshootPortfolio.png" alt="" class="img"',
+  header: 'Keeping track of hundreds of components',
+  techs: ['Ruby on Rails','css','javascript'],
+  descrip: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+}
+const window = document.querySelector('.container')
+const overlay = document.querySelector('.overlay')
+const buttons = document.querySelectorAll('.card button')
+buttons.forEach (button => button.addEventListener(click, () => {
+  popIt(window);
+}));
+
+
+function popIt {
+  if (window == null) return
+  window.classList.add('active');
+  window.classList.add('active');
+}
+
+function clsIt {
+  if (window == null) return
+  window.classList.remove('active');
+  window.classList.remove('active');
+}
+
+
